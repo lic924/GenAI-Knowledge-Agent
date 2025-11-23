c7861fc6f7a2b44a5cc1796ed8c6d6bed739f67d
lic <xvv0924@gmail.com>
2025-11-23 20:59:33 +0800

    Fix import and pinned requirements for Streamlit deployment

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain.vectorstores import Chroma
+from langchain_community.vectorstores.chroma import Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.llms import Ollama
 from langchain.embeddings import OllamaEmbeddings
